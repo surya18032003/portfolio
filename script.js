e7feb855b6e1cc114ba84ae648941ddc4b84d7cb
surya18032003 <suryaramasundaram18@gmail.com>
2024-01-23 11:19:30 +0530

    add portfolio master

---
+++ script.js
@@ -0,0 +1,7 @@
+function showProjectDetails(projectId) {
+    document.getElementById(projectId + 'Modal').style.display = 'block';
+}
+
+function hideProjectDetails(projectId) {
+    document.getElementById(projectId + 'Modal').style.display = 'none';
+}
